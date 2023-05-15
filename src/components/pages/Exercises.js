fd982da2a47541cdf58f90c7b0e4749628fd9790
Andrew Styles <andystyles@hotmail.co.uk>
2023-05-15 21:45:54 +0100

    did some styling to exercises page, made the exercises buttons

--- src/components/pages/Exercises.js
+++ src/components/pages/Exercises.js
@@ -7,11 +7,12 @@ const Exercises = () => {
     
 
     return (
-        <div className="">
-            <div>
+        <div className="w-3/4 h-full mx-auto grid justify-items-center" style={{ gridTemplateRows: '10% 90%' }}>
+            <div className='p-4'>search bar and filter buttons</div>
+            <div className=''>
                 {alphabeticalSort.map((exercise, index) => (
-                <div key={index}>
-                <h2>{exercise.name}</h2>
+                <div className='p-1' key={index}>
+                <button className='w-full border-b-2 border-b-slate-300 pl-2 pr-2 rounded-lg shadow-lg bg-gradient-to-b from-slate-800 to-slate-900'>{exercise.name}</button>
                 </div>
                 ))}
             </div>
